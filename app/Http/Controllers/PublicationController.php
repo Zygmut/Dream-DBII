6223ac21384181cf04b94f9f156d908bdc748abc
Sergi Mayol <sergi2808@gmail.com>
2022-12-06 12:13:54 +0100

    Publicaciones

--- app/Http/Controllers/PublicationController.php
+++ app/Http/Controllers/PublicationController.php
@@ -9,12 +9,68 @@ use Illuminate\Support\Facades\DB;
 
 class PublicationController extends Controller
 {
+    public function index()
+    {
+        // Comprobamos que el usuario esté logueado
+        if (!Session::has('user')) {
+            return redirect('/login');
+        }
+
+        // Obtenemos el usuario logueado
+        $username = Session::get('user')->nombreUsuario;
+
+        //Comprobar si el usuario existe
+        $user = DB::table('info_usuario')->where('nombreUsuario', $username)->first();
+        if (!$user) {
+            return redirect('/login');
+        }
+
+        return view('newpublication');
+    }
+
     public function newcommet($idUsuario, $idPublicacion)
     {
         // TODO: Implementar
     }
 
-    public function newPublication(){
+    public function newPublication($idUsuario)
+    {
+        // Comprobamos que el usuario esté logueado
+        if (!Session::has('user')) {
+            return redirect('/login');
+        }
+
+        // Comprobamos que el usuario que está intentando crear una publicación es el mismo que está logueado
+        if (Session::get('user')->idUsuario != $idUsuario) {
+            return redirect('/login');
+        }
+
+        // Validamos los datos
+        $validator = Validator::make(request()->all(), [
+            'descripcion' => 'required | max:255',
+            'contenido' => 'required | image | mimes:jpeg,png,jpg,gif,svg',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect('/publication/new')->withErrors($validator);
+        }
+
+        // NO FUNCIONA, FALTA SOLUCIONAR COMO OBTENER LA IMAGEN Y GURADARLA EN BASE64
         
+        // Save the image from the content of the request into a blob in the database in base64
+        $image = request()->file('contenido');
+        $base64 = base64_encode($image->getContent());
+        
+
+        // Insert into 'publicacion' (idUsuarioAutor, descripcion, contenido, fecha)
+        // values (idUsuario, descripcion, contenido, fecha)
+        DB::table('publicacion')->insert([
+            'idUsuarioAutor' => $idUsuario,
+            'descripcion' => request()->descripcion,
+            'contenido' => $base64,
+            'fecha' => date('Y-m-d H:i:s'),
+        ]);
+
+        return redirect('/' . Session::get('user')->nombreUsuario . '/profile');
     }
 }
---
+++ resources/views/newpublication.blade.php
@@ -0,0 +1,71 @@
+@extends('layouts.base', ['title' => 'New publication'])
+
+@section('content')
+<div class="container">
+    <div class="row d-flex justify-content-center align-items-center mt-2 pt-2">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">New publication</div>
+                <div class="panel-body
+                        @if ($errors->any())
+                            alert alert-danger
+                        @endif
+                    ">
+                    @if ($errors->any())
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                    @endif
+                    <form class="form-horizontal" method="POST" action="/publication/new/{{ session()->get('user')->idUsuario }}" enctype="multipart/form-data">
+                        {{ csrf_field() }}
+                        <div class="form-group
+                                @if ($errors->has('title'))
+                                    has-error
+                                @endif
+                            ">
+                            <label for="title" class="col-md-4 control-label">Title</label>
+                            <div class="col-md-6">
+                                <input id="title" type="text" class="form-control" name="title" value="{{ old('title') }}" required autofocus>
+                            </div>
+                        </div>
+                        <div class="form-group
+                                @if ($errors->has('description'))
+                                    has-error
+                                @endif
+                            ">
+                            <label for="description" class="col-md-4 control-label">Description</label>
+                            <div class="col-md-6">
+                                <textarea id="description" class="form-control" name="descripcion" required>{{ old('description') }}</textarea>
+                            </div>
+                        </div>
+                        <div class="form-group
+                                @if ($errors->has('image'))
+                                    has-error
+                                @endif
+                            ">
+                            <label for="image" class="col-md-4 control-label">Image</label>
+                            <div class="col-md-6">
+                                <input id="image" type="file" class="form-control" name="contenido" required>
+                            </div>
+                        </div>
+                        <!-- Submit -->
+                        <div class="form-group
+                                @if ($errors->has('submit'))
+                                    has-error
+                                @endif
+                            ">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">
+                                    Create
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
